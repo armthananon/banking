a0633e04ac1a7ec4559e1de649ca7d7a851939f2
armthananon <armza054@gmail.com>
2024-07-22 03:54:52 +0700

    add env loader, implement api and testing with gomock

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,20 +6,20 @@ import (
 	"os"
 	"testing"
 
+	"github.com/armthananon/banking/util"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://myuser:mypassword@localhost:5432/banking?sslmode=disable"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("Can't load config: ", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Can't connect to db : ", err)
 	}
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/armthananon/banking/api"
+	db "github.com/armthananon/banking/db/sqlc"
+	"github.com/armthananon/banking/util"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Can't load config: ", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("Can't connect to db: ", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("Can't start server:", err)
+	}
+}
